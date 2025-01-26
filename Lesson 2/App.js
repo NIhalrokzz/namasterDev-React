dabdcdee482c7d64d28173d74d69deb778e2729e
NIhalrokzz <singhnihal838@gmail.com>
2025-01-26 13:40:46 +0530

    adding lesson2

---
+++ Lesson 2/App.js
@@ -0,0 +1,24 @@
+/*
+*
+<div id="parent">
+    <div id="child">
+        <h1>This is H1</h1>
+        <h2>This is H2</h2>
+    </div>
+</div>
+ReactElement(Object) => HTML(elements) using render
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+    "div", 
+    {id:"heading"},
+    React.createElement(
+        "div",
+        {id: "child"},
+        [React.createElement("h1", {}, "This is H1"), React.createElement("h2", {}, "This is H2")]
+    ));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
